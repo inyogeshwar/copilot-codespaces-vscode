1ad8255ea4529c6a70550c546dea274ae5bbc23b
Yogeshwar Kumar <111217412+inyogeshwar@users.noreply.github.com>
2025-07-08 21:29:34 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+// Create web server
+
+function createWebServer() {
+    const http = require('http');
+    const server = http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello World\n');
+    });
+    server.listen(3000, () => {
+        console.log('Server running at http://localhost:3000/');
+    });
+}
